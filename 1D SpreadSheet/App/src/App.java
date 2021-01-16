977e9a1425e87dd21267d59202fa02409b5a0cd7
meta <gael.lehchibi@outlook.com>
2021-01-16 17:55:51 +0100

    Add 1D SpreadSheets

--- 1D SpreadSheet/App/src/App.java
+++ 1D SpreadSheet/App/src/App.java
@@ -1,10 +1,57 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.math.*;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Scanner scanner = new Scanner(System.in);
-        int solution = Integer.parseInt(scanner.next());
-        System.out.println(solution);
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    private static String[][] sheets;
+    private static Integer[] res;
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);   
+        int N = in.nextInt();
+        sheets = new String[N][3];
+        res = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            String operation = in.next();
+            String arg1 = in.next();
+            String arg2 = in.next();
+            sheets[i] = new String[]{operation, arg1, arg2};
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.println(getResult(i));
+        }
     }
+
+	private static int getResult(int i) {
+        if (res[i] != null) {
+            return res[i];
+        }
+
+        String arg1 = sheets[i][1];
+        String arg2 = sheets[i][2];
+        int val1 = arg1.charAt(0) == '$' ? getResult(Integer.parseInt(arg1.substring(1))) : Integer.parseInt(arg1);
+        int val2 = arg2.equals("_") ? 0 : arg2.charAt(0) == '$' ? getResult(Integer.parseInt(arg2.substring(1))) : Integer.parseInt(arg2);
+        switch (sheets[i][0]) {
+            case "VALUE":
+            res[i] = val1;
+            break;
+            case "ADD":
+            res[i] = val1 + val2;
+            break;
+            case "SUB":
+            res[i] = val1 - val2;
+            break;
+            case "MULT":
+            res[i] = val1 * val2;
+            break;
+            default:
+            break;
+        }
+        return res[i];    
+	}
 }
